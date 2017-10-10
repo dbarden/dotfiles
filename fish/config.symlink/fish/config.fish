c08edff0f95f6ec19cef0fde8f70b24822bb4903
Daniel Barden <daniel.barden@gmail.com>
2017-10-10 06:40:22 +0200

    Add alias for bundle exec

--- fish/config.symlink/fish/config.fish
+++ fish/config.symlink/fish/config.fish
@@ -1,5 +1,9 @@
 set -gx LSCOLORS gxfxcxdxbxegedabagacad
 
+# Ruby setup
 source /usr/local/share/chruby/chruby.fish
 source /usr/local/share/chruby/auto.fish
 chruby ruby-2.4.2
+
+# Aliases
+alias be="bundle exec"
